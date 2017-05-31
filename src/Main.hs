4a48ec00fa44af0f41e1698592bd4b77f24281a5
IQubic <avi.caspe@gmail.com>
2017-05-31 14:43:06 -0700

    Data types for Tic-Tac-Toe pieces and board

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
+import Data.Array
+
+data Piece = X | O | Blank
+
+type Board = Array (Int, Int) Piece
+
+showPiece :: Piece -> Char
+showPiece X = 'X'
+showPiece O = 'O'
+showPiece Blank = ' '
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = putStrLn "hello world"
