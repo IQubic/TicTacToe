051378b9c31a080c793dcca8a3a98775d5e22fdd
IQubic <avi.caspe@gmail.com>
2017-05-31 19:29:36 -0700

    Added support for making and updating a board

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,15 @@ data Piece = X | O | Blank
 
 type Board = Array (Int, Int) Piece
 
+mkNewBoard :: Int -> Board
+mkNewBoard n = listArray ((0, 0), (n - 1, n -1)) (replicate (n*n) Blank)
+
+mkMove :: Board -> Int -> Int -> Piece -> Board
+mkMove state n m piece = state // [((n, m), piece)]
+
+getBoardSize :: Board -> Int
+getBoardSize b = snd (snd $ bounds b) + 1
+
 showPiece :: Piece -> Char
 showPiece X = 'X'
 showPiece O = 'O'
