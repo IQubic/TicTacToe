ca423bb29880f1634a9f0fdc5cfd29964bb8fd24
IQubic <avi.caspe@gmail.com>
2017-06-01 21:21:44 -0700

    Added many more IO functions. Can take a move until valid.

--- src/Main.hs
+++ src/Main.hs
@@ -12,8 +12,11 @@ type Board = Array (Int, Int) Piece
 mkNewBoard :: Int -> Board
 mkNewBoard n = listArray ((0, 0), (n - 1, n -1)) (replicate (n*n) Blank)
 
-mkMove :: Board -> Int -> Int -> Piece -> Board
-mkMove state n m piece = state // [((n, m), piece)]
+mkMove :: Board -> (Int, Int) -> Piece -> Board
+mkMove board spot piece = board // [(spot, piece)]
+
+isValidMove :: Board -> (Int, Int) -> Bool
+isValidMove board spot = board ! spot == Blank
 
 getBoardSize :: Board -> Int
 getBoardSize b = snd (snd $ bounds b) + 1
@@ -34,5 +37,28 @@ parseInput :: Int -> String -> Maybe Int
 parseInput size input = maybeNum >>= \num -> guard (0 <= num && num < size) >> pure num
   where maybeNum = readMaybe input :: Maybe Int
 
+takeInput :: (String -> Maybe a) -> String -> IO a
+takeInput parser errorMsg = do input <- getLine
+                               case parser input of
+                                 Just num -> pure num
+                                 Nothing -> putStr errorMsg >> takeInput parse errorMsg
+
+takeMove :: Int -> IO (Int, Int)
+takeMove size = do putStr "Enter a row: "
+                   row <- takeInput parser (errorMsg "row")
+                   putStr "Enter a col: "
+                   col <- takeInput parser (errorMsg "column")
+                   return (row, col)
+                     where parser = parseInput size
+                           errorMsg s = "Invalid " ++ s ++ ". Try again: "
+
+takeValidMove :: Board -> IO (Int, Int)
+takeValidMove board = do move <- takeMove $ getBoardSize board
+                         if isValidMove board move then return move else putStrLn "Invalid Move. Try Again:" >> takeValidMove board
+
 main :: IO ()
-main = putStrLn "hello world"
+main = do board <- pure $ mkMove (mkNewBoard 3) (1, 1) O
+          putStrLn (showBoard board)
+          move <- takeValidMove board
+          board' <- pure $ mkMove board move X
+          putStrLn (showBoard board')
